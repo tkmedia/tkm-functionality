3a15eed2b1895f5cade1f88c8b0e24f593b8d002
Tal Katz <tal@tkmedia.co.il>
2019-07-04 09:07:30 +0300

    Update acf-blocks.php

--- inc/acf-blocks.php
+++ inc/acf-blocks.php
@@ -8061,7 +8061,7 @@ acf_add_local_field_group(array(
 									'save_other_choice' => 0,
 								),
 								array(
-									'key' => 'field_5tk5cds34e659',
+									'key' => 'field_5tk5cds34e3ddfr3',
 									'label' => __('Icon Size (if font icon)', 'tkm-functionality'),
 									'name' => 'flex_bulletin_list_icon_size',
 									'type' => 'number',
@@ -8084,7 +8084,7 @@ acf_add_local_field_group(array(
 								array(
 									'key' => 'field_5tk54c9dd1fb673',
 									'label' => __('Image icon cut type', 'tkm-functionality'),
-									'name' => 'flex_bulletin_list_icon_size',
+									'name' => 'flex_bulletin_list_size',
 									'type' => 'radio',
 									'instructions' => '',
 									'required' => 0,
