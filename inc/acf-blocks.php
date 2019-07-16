1aa1bcc455f7e06b772fa392274879ff15602463
Tal Katz <tal@tkmedia.co.il>
2019-07-16 23:08:33 +0300

    Update acf-blocks.php

--- inc/acf-blocks.php
+++ inc/acf-blocks.php
@@ -2399,7 +2399,7 @@ acf_add_local_field_group(array(
 									'required' => 0,
 									'conditional_logic' => 0,
 									'wrapper' => array(
-										'width' => '33',
+										'width' => '25',
 										'class' => '',
 										'id' => '',
 									),
